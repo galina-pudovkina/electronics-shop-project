d3f25bbc70ce92ad51dafd3640097c2fa297afb0
Galina Pudovkina <galinapudovkina8@mail.ru>
2023-06-27 19:01:13 +0300

    added tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,7 +1,8 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
 from src.item import Item
 
-item1 = Item("Ноутбук", 20000, 5)
+item1 = Item("Смартфон", 20000, 5)
 
 
 def test_calculate_total_price():
@@ -11,3 +12,11 @@ def test_calculate_total_price():
 def test_apply_discount():
     item1.pay_rate = 0.5
     assert item1.apply_discount() == 10000.0
+
+
+def test_name():
+    assert item1.name == 'Смартфон'
+
+
+def test_string_to_number(test_num):
+    assert Item.string_to_number(test_num) == 5
