27226559ee2a75b1c8541835ad1b7972539dec7f
Galina Pudovkina <galinapudovkina8@mail.ru>
2023-06-29 16:47:22 +0300

    added tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -3,14 +3,14 @@ import pytest
 
 from src.item import Item
 
-item1 = Item("Смартфон", 20000, 5)
-
 
 def test_calculate_total_price():
+    item1 = Item("Смартфон", 20000, 5)
     assert item1.calculate_total_price() == 100000
 
 
 def test_apply_discount():
+    item1 = Item("Смартфон", 20000, 5)
     item1.pay_rate = 0.5
     assert item1.apply_discount() == 10000.0
 
@@ -24,3 +24,13 @@ def test_name():
 
 def test_string_to_number(test_num):
     assert Item.string_to_number(test_num) == 5
+
+
+def test_info1():
+    item1 = Item("Смартфон", 10000, 20)
+    assert repr(item1) == "Item('Смартфон', 10000, 20)"
+
+
+def test_info2():
+    item1 = Item("Смартфон", 10000, 20)
+    assert str(item1) == 'Смартфон'
